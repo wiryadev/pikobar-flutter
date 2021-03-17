694a996c08dd4cf1e6ec37c4bec6b9f909a0a66c
Risa Herdianto <aputra.zha@gmail.com>
2021-03-17 18:03:27 +0700

    Improve native android to support flutter v2

--- android/app/src/main/kotlin/id/go/jabarprov/pikobar/Application.java
+++ android/app/src/main/kotlin/id/go/jabarprov/pikobar/Application.java
@@ -5,20 +5,18 @@ import com.transistorsoft.flutter.backgroundfetch.BackgroundFetchPlugin;
 import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
-import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
 
 public class Application extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        FlutterFirebaseMessagingService.setPluginRegistrant(this);
         BackgroundFetchPlugin.setPluginRegistrant(this);
     }
 
+
     @Override
-    public void registerWith(PluginRegistry pluginRegistry) {
-        FirebaseMessagingPlugin.registerWith(pluginRegistry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+    public void registerWith(PluginRegistry registry) {
+
     }
 }
